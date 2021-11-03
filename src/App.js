b1922dd659e9e00781afcaeee4de041547702a6b
Jake Riordan <contact@jakeriordan.dev>
2021-11-03 09:02:55 +0100

    feat(app): Disable analytics until release

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./variables.css";
 import "./App.css";
 import "./reset.css";
 import { Switch, Route, withRouter, useLocation } from "react-router-dom";
-import Signup from "./pages/Signup";
+import RASO from "pages/RASO";
 import { UserDataProvider } from "./userData";
 import { Fragment, useEffect } from "react";
 import { initIntercom } from "./res/intercom";
@@ -14,45 +14,47 @@ import {
 import { initPixel, logPageView } from "./res/pixel";
 import { initGTM } from "./res/gtag";
 import { country } from "./settings/config";
-import { FAlogEvent } from "./res/fpc";
+
 import { getUtms } from "./res/utms";
-// import { initTapfiliate } from "res/tapfiliate";
+import Header from "components/Common/Header";
+import Footer from "components/Common/Footer";
 
 function App() {
-  useEffect(() => {
-    initIntercom();
-    initAmplitude();
-    initPixel();
-    // initGTM(); // TODO Currently activating chrome Debugger ??
-    // initTapfiliate();
-    FAlogEvent();
-  }, []);
+  // useEffect(() => {
+  //   initIntercom();
+  //   initAmplitude();
+  //   initPixel();
+  //   initGTM(); // TODO Currently activating chrome Debugger ??
 
-  const location = useLocation();
-  useEffect(() => {
-    logPageView();
+  // }, []);
 
-    const utms = getUtms();
-    sendAmplitudeData("WEB_SIGNUP_PAGEVIEW", {
-      path: location.pathname,
-      country,
-      ...utms,
-    });
-    setAmplitudeUserProperties({
-      utms,
-      ...utms,
-      user_country: country,
-    });
-  }, [location.pathname, location.search]);
+  // const location = useLocation();
+  // useEffect(() => {
+  //   logPageView();
+
+  //   const utms = getUtms();
+  //   sendAmplitudeData("WEB_SIGNUP_PAGEVIEW", {
+  //     path: location.pathname,
+  //     country,
+  //     ...utms,
+  //   });
+  //   setAmplitudeUserProperties({
+  //     utms,
+  //     ...utms,
+  //     user_country: country,
+  //   });
+  // }, [location.pathname, location.search]);
 
   return (
     <UserDataProvider>
+      <Header />
       <Switch>
         <Fragment>
-          <Route exact path="/" component={Signup} />
-          <Route exact path="/en" component={Signup} />
+          <Route exact path="/" component={RASO} />
+          <Route exact path="/en" component={RASO} />
         </Fragment>
       </Switch>
+      <Footer />
     </UserDataProvider>
   );
 }
