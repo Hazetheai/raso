8c5e78bc36270947c90070d3da053c1ba07f9d09
Jake Riordan <contact@jakeriordan.dev>
2021-11-10 11:40:04 +0100

    feat(Link):Change to foward ref

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -4,29 +4,50 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import clsx from "clsx";
 
-const Link = ({ children, href, text, title, func, className, ...rest }) => {
-  return (
-    <a
-      {...rest}
-      href={href}
-      title={title}
-      className={clsx("link", className)}
-      onClick={func}
-      onKeyDown={(e) => {
-        if (
-          e.key === "Enter" ||
-          e.code === "Enter" ||
-          e.key === " " ||
-          e.code === "Space"
-        ) {
-          func && func(e);
-        }
-      }}
-    >
-      {children || text}{" "}
-      <FontAwesomeIcon className="faq-icon" icon={faArrowRight} />
-    </a>
-  );
-};
+const Link = React.forwardRef(
+  (
+    {
+      children,
+      href,
+      text,
+      title,
+      func,
+      className,
+      secondary,
+      inline,
+      ...rest
+    },
+    ref
+  ) => {
+    return (
+      <a
+        {...rest}
+        ref={ref}
+        href={href}
+        title={title}
+        className={clsx(
+          "link",
+          className,
+          secondary && "link--sencondary",
+          inline && "link--inline"
+        )}
+        onClick={func}
+        onKeyDown={(e) => {
+          if (
+            e.key === "Enter" ||
+            e.code === "Enter" ||
+            e.key === " " ||
+            e.code === "Space"
+          ) {
+            func && func(e);
+          }
+        }}
+      >
+        {children || text}{" "}
+        <FontAwesomeIcon className="faq-icon" icon={faArrowRight} />
+      </a>
+    );
+  }
+);
 
 export default Link;
