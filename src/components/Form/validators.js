a0520ffb01adb2141aeb5802ba272faa9e21c7a7
Jake Riordan <contact@jakeriordan.dev>
2021-11-10 11:44:36 +0100

    feat(Form) Update Validators

--- src/components/Form/validators.js
+++ src/components/Form/validators.js
@@ -1,19 +1,42 @@
-import { host } from "settings/config";
+import qs from "qs";
+import { accountableApp, wpHost } from "settings/config";
+
 export const emailValidator = async (value) => {
-  const response = await fetch(
-    `${host}/api/v1/users/emailcheck?email=${value.toLowerCase()}`,
-    {
-      method: "GET",
-      cache: "default",
-    }
-  );
-  const json = await response.json();
-  return !json.exists;
+  try {
+    const response = await fetch(
+      `${accountableApp}/api/v1/users/emailcheck?email=${value.toLowerCase()}`,
+      {
+        method: "GET",
+        cache: "default",
+      }
+    );
+
+    const json = await response.json();
+    return !json.exists;
+  } catch (err) {
+    return false;
+  }
+};
+export const taxIdValidator = async (value) => {
+  try {
+    const response = await fetch(`${wpHost}/raso-api/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      },
+      body: qs.stringify({ taxId: value }),
+    });
+
+    const json = await response.json();
+    return json.valid || "Dieses Feld ist ungültig";
+  } catch (err) {
+    return "There was an error validating the Tax ID. Please try again later";
+  }
 };
 
 export let isTaxIdValid = true;
-export const moneyRegex = /^€ (?=.{1,12}$)(-(?=.*[1-9].*))?(?!0\d)\d{1,12}$/;
-export let validators = {
+export const moneyRegex = /(\d*)(?:\.?)(\d+),?(\d{0,2}) ?€?$/;
+export const validators = {
   firstname: /^.{1,72}$/,
   name: /^.{1,72}$/,
   email: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
@@ -113,3 +136,113 @@ export const elsterStNrFormatter = {
     replace: "9$10$2",
   },
 };
+
+export function isValidIBANNumber(input) {
+  if (input === "") return 1; //empty ok
+  var CODE_LENGTHS = {
+    AD: 24,
+    AE: 23,
+    AT: 20,
+    AZ: 28,
+    BA: 20,
+    BE: 16,
+    BG: 22,
+    BH: 22,
+    BR: 29,
+    CH: 21,
+    CR: 21,
+    CY: 28,
+    CZ: 24,
+    DE: 22,
+    DK: 18,
+    DO: 28,
+    EE: 20,
+    ES: 24,
+    FI: 18,
+    FO: 18,
+    FR: 27,
+    GB: 22,
+    GI: 23,
+    GL: 18,
+    GR: 27,
+    GT: 28,
+    HR: 21,
+    HU: 28,
+    IE: 22,
+    IL: 23,
+    IS: 26,
+    IT: 27,
+    JO: 30,
+    KW: 30,
+    KZ: 20,
+    LB: 28,
+    LI: 21,
+    LT: 20,
+    LU: 20,
+    LV: 21,
+    MC: 27,
+    MD: 24,
+    ME: 22,
+    MK: 19,
+    MR: 27,
+    MT: 31,
+    MU: 30,
+    NL: 18,
+    NO: 15,
+    PK: 24,
+    PL: 28,
+    PS: 29,
+    PT: 25,
+    QA: 29,
+    RO: 24,
+    RS: 22,
+    SA: 24,
+    SE: 24,
+    SI: 19,
+    SK: 24,
+    SM: 27,
+    TN: 24,
+    TR: 26,
+  };
+  var iban = String(input)
+      .toUpperCase()
+      .replace(/[^A-Z0-9]/g, ""), // keep only alphanumeric characters
+    code = iban.match(/^(DE)(\d{2})([A-Z\d]+)$/), // match and capture (1) the country code, (2) the check digits, and (3) the rest
+    digits;
+  // check syntax and length
+  if (!code || iban.length !== CODE_LENGTHS[code[1]]) {
+    return false;
+  }
+  // rearrange country code and check digits, and convert chars to ints
+  digits = (code[3] + code[1] + code[2]).replace(/[A-Z]/g, function (letter) {
+    return letter.charCodeAt(0) - 55;
+  });
+  // final check
+  return mod97(digits);
+}
+
+function mod97(string) {
+  var checksum = string.slice(0, 2),
+    fragment;
+  for (var offset = 2; offset < string.length; offset += 7) {
+    fragment = String(checksum) + string.substring(offset, offset + 7);
+    checksum = parseInt(fragment, 10) % 97;
+  }
+  return checksum;
+}
+
+function validateValue(k, v, fields) {
+  switch (k) {
+    case "steuernummer_value":
+      return elsterStNrFormatter[fields.steuernummer_state].match.test(
+        v.trim().replace(/\/|\s/g, "")
+      );
+    case "steueridentifkationsnummer":
+      return isTaxIdValid;
+    case "phone":
+      return /^\+\d{11,15}$/.test(v.trim().replace(/\s/g, ""));
+    default:
+      if (validators[k]) return validators[k].test(v.trim());
+      else return true;
+  }
+}
