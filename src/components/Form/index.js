5c32ad28d00fcab124ecc8674465af750fb7b570
Jake Riordan <contact@jakeriordan.dev>
2021-11-10 11:41:57 +0100

    Chore: Rename File

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,99 +1,157 @@
-import React, { useState, Fragment, useEffect } from "react";
-import "./form.css";
-import Personal from "./Personal";
-import Business from "./Business";
-import Start from "./Start";
-import Tabs from "./Tabs";
-import { Redirect } from "react-router-dom";
-import { useUserData } from "userData";
-import { country, host } from "settings/config";
+import Tabs, { sampleTabData } from "components/Tabs";
+import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
-import { setIntercomEmail } from "res/intercom";
-import { setAmplitudeUserId } from "res/amplitude";
-import { emailValidator } from "./validators";
-import { signup } from "./signup";
+import { removeDuplicates } from "res/lib";
+import { useUserData } from "userData";
+import { useUserInteraction } from "userInteraction";
+import BankAccount from "./BankAccount";
+import Business from "./Business";
+import "./form-layout.css";
+import "./form.css";
+import {
+  formatDatasection,
+  reFormatForFormData,
+  sanitizeNumbers,
+  wrap,
+} from "./helpers";
+import Personal from "./Personal";
+import Review from "./Review";
+import TaxEstimate from "./TaxEstimate";
+import TaxInfo from "./TaxInfo";
 
-const Form = () => {
-  const { userData, setUserData } = useUserData(); //set UserData for thank you modal
-  const [step, setStep] = useState(0); //display this step
-  const [status, setStatus] = useState(""); // status either loading/failed/empty/redirect
-  const [businessInfoList, setbusinessInfoList] = useState([]);
-  const [formData, setFormData] = useState({}); // Contains all the form data from the steps
-  const { t } = useTranslation();
-  useEffect(() => {
-    if (userData.rasoRef) {
-      (async () => {
-        const emailExists = await emailValidator(userData.email);
-        if (!emailExists) setStatus("redirect");
-        return;
-      })();
-    }
-    if (userData.promocode)
-      setFormData({ ...formData, pomocode: userData.promocode });
-  }, [userData]);
+const steps = sampleTabData.tabs.slice(0);
 
-  /**
-   *
-   * @param {*} data
-   * @param {"personalFields"|"businessFields"|"taxInfoFields"|"taxEstimateFields"|"bankAccountFields"|"reviewFields"} dataSection
-   */
-
-  const nextStep = async (data, dataSection) => {
-    // Go to next step if last step submit to backend
-    const newData = { ...formData, ...data, language: t("current_language") };
-    // setFormData(newData);
-
-    setUserData(data, dataSection, true);
-
-    if (step + 1 < steps.length) {
-      if (step === 0) {
-        setIntercomEmail(data.email, t("current_language"));
-        setAmplitudeUserId(data.email);
-      }
-
-      setStep(step + 1);
-    } else {
-      setStatus("loading");
-      // TODO Do we want to check if they have signed up with us before?
-      // const emailExists = await emailValidator(newData.email);
-      // if (!emailExists) {
-      //   setStatus("redirect");
-      //   return;
-      // }
-      // const signedUp = await signup(newData);
-
-      // if (signedUp) setStatus("redirect");
-      // else setStatus("failed");
-    }
-  };
-  const steps = [
-    <Personal nextStep={nextStep} />,
-    <Business
-      nextStep={nextStep}
-      businessInfoList={businessInfoList}
-      formData={formData}
-      disabled={status === "loading" ? true : false}
-      error={status === "failed" ? true : false}
-    />,
+function calcNextStep(steps, currentStep) {
+  return steps[
+    wrap(
+      steps.map((el) => el.tabId).indexOf(currentStep.tabId) + 1,
+      0,
+      steps.length - 1
+    )
   ];
-  const getBusinessInfo = async (name) => {
-    const response = await fetch(
-      `${host}/api/v1/search-enterprises?name=${name}`
+}
+
+const FormLayout = ({ defaultSteps = steps }) => {
+  const { userInteraction, setUserInteraction } = useUserInteraction();
+  const [steps, setSteps] = useState(defaultSteps);
+  const [currentStep, setCurrentStep] = useState(
+    userInteraction.workingStep
+      ? defaultSteps.find((s) => s.tabId === userInteraction.workingStep)
+      : defaultSteps[0]
+  );
+  const { t } = useTranslation();
+  const layoutRef = useRef(null);
+  const { userData, setUserData } = useUserData();
+
+  useEffect(() => {
+    if (steps.indexOf(currentStep) > 0)
+      layoutRef.current.scrollIntoView({ behaviour: "smooth" });
+  }, [layoutRef, currentStep, steps]);
+
+  function nextStep(data, dataSection, progress = true) {
+    setUserData(formatDatasection(data), dataSection, true);
+    if (!progress) {
+      return;
+    }
+    setSteps(
+      steps.map((el) =>
+        el.complete === true
+          ? { ...el }
+          : { ...el, complete: el.tabId === dataSection }
+      )
     );
-    return await response.json();
-  };
+    setCurrentStep(calcNextStep(steps, currentStep));
+
+    setUserInteraction({
+      stepsCompleted: removeDuplicates([
+        ...steps.filter((s) => s.complete).map((s) => s.tabId),
+        currentStep.tabId,
+      ]),
+      workingStep: calcNextStep(steps, currentStep).tabId,
+    });
+  }
+
+  function handleTabClick(id) {
+    const selectedTab = steps.find((el) => el.tabId === id);
+    if (selectedTab.complete) {
+      setCurrentStep(selectedTab);
+    }
+  }
+
   return (
-    <>
-      <Tabs step={step} />
-
-      {steps[step]}
-
-      {status === "redirect" && (
-        <Redirect
-          to={country === "de" ? t("thank_you_page_de") : t("thank_you_page")}
-        />
-      )}
-    </>
+    <div ref={layoutRef} className="form-layout">
+      <Tabs
+        tabData={{ ...sampleTabData, tabs: steps }}
+        activeTab={userInteraction?.tabId || currentStep.tabId}
+        onTabClick={handleTabClick}
+      />
+      <div className="form-container element-container">
+        <div className="screen-header">
+          <h2 className="screen-title">{currentStep.tabLabel}</h2>
+          <p className="screen-subtitle body--small">
+            {currentStep.tabSubtitle}
+          </p>
+          {currentStep.tabHelper && (
+            <p className="tab-helper body--medium">{currentStep.tabHelper}</p>
+          )}
+        </div>
+        {currentStep.tabId === "personalFields" && (
+          <Personal
+            steps={steps}
+            currentStep={currentStep}
+            comingStep={calcNextStep(steps, currentStep)}
+            nextStep={nextStep}
+            defaultValues={reFormatForFormData(userData.personalFields)}
+          />
+        )}
+        {currentStep.tabId === "businessFields" && (
+          <Business
+            steps={steps}
+            currentStep={currentStep}
+            comingStep={calcNextStep(steps, currentStep)}
+            nextStep={nextStep}
+            defaultValues={reFormatForFormData(userData.businessFields)}
+          />
+        )}
+        {currentStep.tabId === "taxInfoFields" && (
+          <TaxInfo
+            steps={steps}
+            currentStep={currentStep}
+            comingStep={calcNextStep(steps, currentStep)}
+            nextStep={nextStep}
+            defaultValues={reFormatForFormData(userData.taxInfoFields)}
+          />
+        )}
+        {currentStep.tabId === "taxEstimateFields" && (
+          <TaxEstimate
+            steps={steps}
+            currentStep={currentStep}
+            comingStep={calcNextStep(steps, currentStep)}
+            nextStep={nextStep}
+            defaultValues={reFormatForFormData(userData.taxEstimateFields)}
+          />
+        )}
+        {currentStep.tabId === "bankAccountFields" && (
+          <BankAccount
+            steps={steps}
+            currentStep={currentStep}
+            comingStep={calcNextStep(steps, currentStep)}
+            nextStep={nextStep}
+            defaultValues={reFormatForFormData(userData.bankAccountFields)}
+          />
+        )}
+        {currentStep.tabId === "reviewFields" && (
+          <Review
+            steps={steps}
+            currentStep={currentStep}
+            comingStep={calcNextStep(steps, currentStep)}
+            nextStep={nextStep}
+            defaultValues={reFormatForFormData(userData.reviewFields)}
+          />
+        )}
+      </div>
+    </div>
   );
 };
-export default Form;
+
+export default FormLayout;
