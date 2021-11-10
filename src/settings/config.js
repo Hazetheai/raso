f4149ceac1113409d536149d2805d3831e617824
Jake Riordan <contact@jakeriordan.dev>
2021-11-10 11:26:22 +0100

    feat(app): Update settings

--- src/settings/config.js
+++ src/settings/config.js
@@ -1,15 +1,14 @@
-//DE
-export const host = "https://app.accountable.eu";
+export const isDev = /localhost/gi.test(window.location.href);
+
+export const appHost = isDev
+  ? "http://localhost:3000"
+  : "https://accountable.de";
+export const wpHost = isDev
+  ? "http://localhost:10023"
+  : "https://accountable.de";
+export const accountableApp = "https://app.accountable.eu";
 export const intercomId = "c9wnmh12"; //c9wnmh12 for prod
 export const amplitudeKey = "068667cf512ab58a86f8123c64bfdee6"; // 068667cf512ab58a86f8123c64bfdee6 for prod
 export const fbPixelId = "2207534789479329"; //its prod
 export const gtagId = "GTM-5SW8SVC";
 export const country = "de";
-
-//DE
-// export const host = 'https://helium.app.accountable.eu';
-// export const intercomId = 'llxhxzz5'; //c9wnmh12 for prod
-// export const amplitudeKey = '32bc5dcaf02c179823553bea10a420fa'; // 068667cf512ab58a86f8123c64bfdee6 for prod
-// export const fbPixelId = '2207534789479329'; //its prod
-// export const gtagId = 'GTM-5SW8SVC';
-// export const country = 'de';
