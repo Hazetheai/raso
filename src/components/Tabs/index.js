c8819f31924222fbf0449a572af0a4fdd099c95c
Jake Riordan <contact@jakeriordan.dev>
2021-11-17 10:04:57 +0100

    feat(Components): Remove comments

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -3,66 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import clsx from "clsx";
 import React from "react";
 import home from "res/images/home.svg";
+import { useUserInteraction } from "userInteraction";
 import "./tabs.css";
 
-// export const sampleTabData = {
-//   title: "Wilkommen",
-//   icon: home,
-//   tabs: [
-//     {
-//       tabNumber: "1",
-//       tabLabel: "Persönliche Daten",
-//       tabSubtitle:
-//         "Alle Daten werden SSL-verschlüsselt und über die ELSTER Schnittstelle sicher an dein Finanzamt übertragen.",
-//       tabId: "personalFields",
-//       complete: false,
-//     },
-//     {
-//       tabNumber: "2",
-//       tabLabel: "Geschäftliche Daten",
-//       tabSubtitle:
-//         "Alle Daten werden SSL-verschlüsselt und über die ELSTER Schnittstelle sicher an dein Finanzamt übertragen.",
-//       tabId: "businessFields",
-//       complete: false,
-//     },
-//     {
-//       tabNumber: "3",
-//       tabLabel: "Steuerliche Angaben",
-//       tabSubtitle:
-//         "Alle Daten werden SSL-verschlüsselt und über die ELSTER Schnittstelle sicher an dein Finanzamt übertragen.",
-//       tabId: "taxInfoFields",
-//       complete: false,
-//     },
-//     {
-//       tabNumber: "4",
-//       tabLabel: "Steuerschätzung",
-//       tabSubtitle: "",
-//       tabHelper: `Diese Angaben bilden die Berechnungsgrundlage für deine vierteljährlichen Vorauszahlungen zur Einkommensteuer und Gewerbesteuer.
-//         Bitte wähle mindestens eine Einkommensquelle aus.
-//         `,
-//       tabId: "taxEstimateFields",
-//       complete: false,
-//     },
-//     {
-//       tabNumber: "5",
-//       tabLabel: "Bankkonto",
-//       tabSubtitle:
-//         "Alle Daten werden SSL-verschlüsselt und über die ELSTER Schnittstelle sicher an dein Finanzamt übertragen.",
-//       tabId: "bankAccountFields",
-//       complete: false,
-//     },
-//     {
-//       tabNumber: "6",
-//       tabLabel: "Prüfen & abschicken",
-//       tabSubtitle:
-//         "Alle Daten werden SSL-verschlüsselt und über die ELSTER Schnittstelle sicher an dein Finanzamt übertragen.",
-//       tabId: "reviewFields",
-//       complete: false,
-//     },
-//   ],
-// };
-
 const Tabs = ({ tabData, activeTab, onTabClick }) => {
+  const { userInteraction } = useUserInteraction();
+  const newActiveTab = userInteraction.workingStep || activeTab;
+
+  function filterTabData(tabData) {
+    const newTabData = Object.assign({}, tabData);
+
+    newTabData["tabs"] = newTabData.tabs.map((tab) =>
+      userInteraction.stepsCompleted.includes(tab.tabId)
+        ? { ...tab, complete: true }
+        : tab
+    );
+
+    return newTabData;
+  }
+
   return (
     <div className="tab-container element-container">
       <nav className="tab-nav">
@@ -76,7 +35,7 @@ const Tabs = ({ tabData, activeTab, onTabClick }) => {
         </div>
         <hr />
         <ul className="tab-nav__menu">
-          {tabData.tabs.map((tab) => {
+          {filterTabData(tabData).tabs.map((tab) => {
             const { tabNumber, tabLabel, tabId } = tab;
 
             return (
@@ -86,7 +45,7 @@ const Tabs = ({ tabData, activeTab, onTabClick }) => {
                 key={tabId}
                 className={clsx(
                   "tab-nav__tab",
-                  activeTab === tabId && "active",
+                  newActiveTab === tabId && "active",
                   tab.complete && "complete"
                 )}
                 id={tabId}
@@ -101,7 +60,7 @@ const Tabs = ({ tabData, activeTab, onTabClick }) => {
                 <span
                   className={clsx(
                     "tab-nav__nav-label body--medium",
-                    activeTab === tabId && "body--bold"
+                    newActiveTab === tabId && "body--bold"
                   )}
                 >
                   {tabLabel}
