c77f4878739735eab59dee0f4d57a891bce50879
Jake Riordan <contact@jakeriordan.dev>
2021-11-17 09:51:01 +0100

    feat(SP): Add page file

--- src/pages/RASO/success.js
+++ src/pages/RASO/success.js
@@ -1,11 +1,21 @@
+import Button from "components/Button";
+import CTA from "components/CTA";
+import SecondaryCTA from "components/CTA/SecondaryCTA";
+import FAQ from "components/FAQ";
+import RASOSuccessBSection from "components/Misc/RASOSuccessBSection";
+import FinanzamtLetters from "components/PDFReader/FinanzamtLetters";
 import React from "react";
 import { useTranslation } from "react-i18next";
-import rasoCTAImage from "res/images/raso-cta-2-illustration.png";
+import { GTMsendEvent } from "res/gtag";
 import checkmark from "res/images/checkmark.svg";
-import CTA from "components/CTA";
+import rasoCTAImage from "res/images/raso-cta-2-illustration.png";
+import { useUserData } from "userData";
+import { useUserInteraction } from "userInteraction";
 
 const SuccessPage = () => {
   const { t } = useTranslation();
+  const { userInteraction, setUserInteraction } = useUserInteraction();
+  const { userData, setUserData } = useUserData();
 
   const ctaData = {
     preHeadline: t("success_cta_join_pre_headline"),
@@ -13,19 +23,54 @@ const SuccessPage = () => {
     headline: t("success_cta_join_headline"),
     subheadline: t("success_cta_join_sub_headline"),
     mobileActionElementText: t("success_cta_join_mobile_action_element_text"),
-    mobileActionElementLink: t("success_cta_join_mobile_action_element_link"),
+    mobileActionElementLink: userInteraction.downloadAppLink_mobile,
     desktopActionElementText: t("success_cta_join_desktop_action_element_text"),
-    desktopActionElementLink: t("success_cta_join_desktop_action_element_link"),
+    desktopActionElementLink:
+      userInteraction.downloadAppLink_desktop ??
+      "http://onboarding.accountable.de/en/",
     isButton: false,
-    action: () => console.log("Click CTA"),
+    action: () => GTMsendEvent("RASO_CLICKED_DOWNLOADAPP"),
     offerPromises: [
       { imgSrc: checkmark, text: t("success_cta_join_offer_promises_text_1") },
       { imgSrc: checkmark, text: t("success_cta_join_offer_promises_text_2") },
     ],
   };
+
+  const faqData = {
+    containerClassName: "raso-success-faq",
+    title: "FAQ ⭐️ ",
+    questions: new Array(8).fill({}).map((q, idx) => ({
+      title: t(`raso_success_a_section_faq_${idx + 1}_title`),
+      xerius_number: idx,
+      answer: t(`raso_success_a_section_faq_${idx + 1}_answer`, {
+        interpolation: { escapeValue: false },
+      }),
+    })),
+  };
+
   return (
-    <div>
-      <CTA {...ctaData} />
+    <div className="content">
+      <Button
+        text={userInteraction.version === "a" ? "Set B" : "Set A"}
+        func={() =>
+          setUserInteraction({
+            version: userInteraction.version === "a" ? "b" : "a",
+          })
+        }
+      />
+      {userInteraction.version === "a" && (
+        <>
+          <FinanzamtLetters
+            finanzamtLetters={userData.reviewFields?.finanzamtLetters || []}
+          />
+          <SecondaryCTA />
+          <CTA {...ctaData} />
+
+          <FAQ faqData={faqData} />
+        </>
+      )}
+
+      {userInteraction.version === "b" && <RASOSuccessBSection />}
     </div>
   );
 };
