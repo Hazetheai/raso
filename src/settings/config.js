ca02d8488ef00bcb1570d1fbb0fd90ea9c448083
Jake Riordan <contact@jakeriordan.dev>
2021-11-17 10:05:44 +0100

    Remove proxy

--- src/settings/config.js
+++ src/settings/config.js
@@ -5,10 +5,11 @@ export const appHost = isDev
   : "https://accountable.de";
 export const wpHost = isDev
   ? "http://localhost:10023"
-  : corsProxy + "https://accountable.de";
+  : "https://accountable.de";
 export const accountableApp = "https://app.accountable.eu";
 export const intercomId = "c9wnmh12"; //c9wnmh12 for prod
 export const amplitudeKey = "068667cf512ab58a86f8123c64bfdee6"; // 068667cf512ab58a86f8123c64bfdee6 for prod
 export const fbPixelId = "2207534789479329"; //its prod
 export const gtagId = "GTM-5SW8SVC";
 export const country = "de";
+export const dlAppLink = "https://accountable.app.link/FTshY7oGF3?";
