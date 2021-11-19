9912fcb4bad781da44573aed9c8988dddc98cd3a
Jake Riordan <contact@jakeriordan.dev>
2021-11-19 10:25:21 +0100

    chore: Add lib fns

--- src/res/lib.js
+++ src/res/lib.js
@@ -5,3 +5,24 @@ export function removeDuplicates(array) {
 export function isEmpty(object) {
   return Object.keys(object).length === 0;
 }
+
+export function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+export function cleanLocal() {
+  localStorage.removeItem("personalFields");
+  localStorage.removeItem("businessFields");
+  localStorage.removeItem("taxInfoFields");
+  localStorage.removeItem("taxEstimateFields");
+  localStorage.removeItem("bankAccountFields");
+  localStorage.removeItem("reviewFields");
+  localStorage.removeItem("userData");
+  localStorage.removeItem("userInteraction");
+}
