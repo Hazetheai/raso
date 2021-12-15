6a20d7ba90ef1d39e4ae0060dafb658c223327aa
Jake Riordan <contact@jakeriordan.dev>
2021-12-15 12:14:15 +0100

    fix(hook): Persist data on page refresh

--- src/components/hooks/useLocalState.js
+++ src/components/hooks/useLocalState.js
@@ -1,6 +1,7 @@
 import { isEqual } from "lodash";
 import { useEffect } from "react";
 import { debounce, isEmpty } from "res/lib";
+import { isDev } from "settings/config";
 import { useUserData } from "userData";
 import { useUserInteraction } from "userInteraction";
 import {
@@ -17,6 +18,7 @@ export function useLocalFormVal({ key, reset, localFormVals, errors }) {
   const dbDeactivateAutoSave = debounce(
     () =>
       setTimeout(() => {
+        isDev && console.log(`deactivating autosave`);
         setUserInteraction({ isAutoSaving: false });
       }, 1000),
     300
@@ -24,20 +26,24 @@ export function useLocalFormVal({ key, reset, localFormVals, errors }) {
 
   //   reset to local form values (Step has not been completed)
   useEffect(() => {
+    isDev && console.log(`calling 1st hook`, userInteraction);
     if (
       !userInteraction.stepsCompleted.includes(key) &&
       !userInteraction.touchedScreens.includes(key)
     ) {
+      isDev && console.log(`resetting to userData`, userData[key]);
       reset(reFormatForFormData(userData[key]));
       return;
     }
     if (!userInteraction.stepsCompleted.includes(key) && ls) {
+      isDev && console.log(`resetting to localstorage`);
       reset(reFormatForFormData(JSON.parse(ls)));
     }
   }, []);
 
   // Save local changes to completed steps automatically
   useEffect(() => {
+    isDev && console.log(`calling 2nd hook`);
     if (
       isEmpty(errors) &&
       userInteraction.stepsCompleted.includes(key) &&
@@ -46,6 +52,7 @@ export function useLocalFormVal({ key, reset, localFormVals, errors }) {
         formatDatasection({ ...userData[key], ...localFormVals })
       )
     ) {
+      isDev && console.log(`setting autosave changes`);
       setUserInteraction({
         isAutoSaving: true,
         // Reset Review screen to force regeneration of the pdf
@@ -65,18 +72,27 @@ export function useLocalFormVal({ key, reset, localFormVals, errors }) {
         key,
         true
       );
+
+      dbDeactivateAutoSave();
+    }
+
+    if (userInteraction.isAutoSaving) {
       dbDeactivateAutoSave();
     }
   }, [reset, localFormVals]);
 
   // Save the local form Values
   useEffect(() => {
+    isDev && console.log(`calling 3rd hook`);
+    // no local storage for key => set key
     if (!ls) {
+      isDev && console.log(`setting local storage with userData`);
       localStorage.setItem(
         key,
         JSON.stringify(reFormatForFormData(userData[key]))
       );
     }
+    // Userdata and local storage do not match current form state
     if (
       !isEqual(
         userData[key],
@@ -85,14 +101,16 @@ export function useLocalFormVal({ key, reset, localFormVals, errors }) {
       !isEqual(localFormVals, ls ? JSON.parse(ls) : {}) &&
       !isEmpty(localFormVals)
     ) {
+      isDev && console.log(`setting local storage localFormVals`);
       localStorage.setItem(key, JSON.stringify(localFormVals));
     }
   }, [localFormVals]);
 
   //   reset to User Data form values (Step has been completed)
   useEffect(() => {
+    isDev && console.log(`calling 4th hook`);
     if (userInteraction.stepsCompleted.includes(key)) {
       reset(reFormatForFormData(userData[key]));
     }
-  }, []);
+  }, [userInteraction.stepsCompleted]);
 }
